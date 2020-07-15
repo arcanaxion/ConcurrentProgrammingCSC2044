54d3204d3ded4019618a12a48230de8de3105f1d
arcanaxion <arcanaxion@gmail.com>
2020-07-15 23:46:15 +0800

    changes to assignment

---
+++ Assignment_1/src/attempt_1/App.java
@@ -0,0 +1,25 @@
+package attempt_1;
+
+class Aircraft implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class Runway {
+
+}
+
+public class App {
+    public static void main(String[] args) {
+        int runtimeMs = 120;
+        double aircraftGenChance = 0.2;
+        int aircraftTakeoffMs = 5;
+        int aircraftLandingMs = 10;
+        int avgFlightDuration = 10;
+
+        
+    }
+}
